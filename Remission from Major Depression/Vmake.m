df456c503b5b100a7ae3b71c3054e56bff168689
Ole <31667950+OleAd@users.noreply.github.com>
2019-03-11 11:48:28 +0100

    Removed files from original repository
    Removed files that are not in use in this version of LEiDA.

--- Remission from Major Depression/Vmake.m
+++
@@ -1,6 +0,0 @@
-V=Kmeans_results{5}.C(1,:)
-figure
-plot_nodes_in_cortex(V)
-
-
-V=Kmeans_results{10}.C(8,:)
--- adif.m
+++
@@ -1,6 +0,0 @@
-function c=adif(a,b)
- if abs(a-b)>pi
-  c=2*pi-abs(a-b);
- else
-  c=abs(a-b);
- end
